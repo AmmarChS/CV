3c33caea5abff23db2e1f3c14404b626c2d8f378
AmmarChS <97284954+AmmarChS@users.noreply.github.com>
2022-11-06 21:48:03 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// const menu = document.getElementById("menu");
+const action = document.getElementById("actions");
+
+
+menu.addEventListener("click", () => {
+    hundlemune();
+})
+
+function hundlemune(){
+  // menu.classList.toggle("isactive");
+  action.classList.toggle("isactive");
+}
+
